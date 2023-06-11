575f45b96818302a4bef70dd2bc8c35593b69f5c
Jakub <jakubfirlejczyk@gmail.com>
2023-06-11 19:45:40 +0200

    rmv yml

--- src/test/java/com/example/LearningBlog/integration/BlogUserIT.java
+++ src/test/java/com/example/LearningBlog/integration/BlogUserIT.java
@@ -6,21 +6,14 @@ import com.example.LearningBlog.conteiners.Testcontainers;
 import com.example.LearningBlog.security.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-
 import java.util.Optional;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- src/test/java/com/example/LearningBlog/integration/CommentIT.java
+++ src/test/java/com/example/LearningBlog/integration/CommentIT.java
@@ -2,7 +2,7 @@ package com.example.LearningBlog.integration;
 
 import com.example.LearningBlog.blogUser.*;
 
-import com.example.LearningBlog.conteiners.ContainersTest;
+
 import com.example.LearningBlog.conteiners.Testcontainers;
 import com.example.LearningBlog.post.PostRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,10 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
-import org.springframework.test.context.TestPropertySource;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 
